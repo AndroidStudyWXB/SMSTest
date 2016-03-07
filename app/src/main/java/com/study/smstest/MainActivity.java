4232ec99b5b31c9be942c4faedb7303622827ee2
WXB506 <1922686887@qq.com>
2016-03-07 23:55:30 +0800

    send message

--- app/src/main/java/com/study/smstest/MainActivity.java
+++ app/src/main/java/com/study/smstest/MainActivity.java
@@ -1,14 +1,22 @@
 package com.study.smstest;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Objects;
 
@@ -18,6 +26,11 @@ public class MainActivity extends AppCompatActivity {
     private TextView content;
     private IntentFilter receiveFilter;
     private MessageReceiver messageReceiver;
+    private EditText to;
+    private EditText msgInput;
+    private Button send;
+    private IntentFilter sendFilter;
+    private SendStatusReceiver sendStatusReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +39,30 @@ public class MainActivity extends AppCompatActivity {
 
         sender = (TextView) findViewById(R.id.sender);
         content = (TextView) findViewById(R.id.content);
+        to = (EditText) findViewById(R.id.to);
+        msgInput = (EditText) findViewById(R.id.msg_input);
         receiveFilter = new IntentFilter();
         receiveFilter.addAction("android.provider.Telephony.SMS_RECEIVED");
-        messageReceiver = new MessageReceiver();
+        sendFilter = new IntentFilter();
+        sendFilter.addAction("SENT_SMS_ACTION");
 
+        messageReceiver = new MessageReceiver();
         registerReceiver(messageReceiver, receiveFilter);
+
+        sendStatusReceiver = new SendStatusReceiver();
+        registerReceiver(sendStatusReceiver, sendFilter);
+
+        send = (Button) findViewById(R.id.send);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SmsManager smsManager = SmsManager.getDefault();
+                Intent sendIntent = new Intent("SENT_SMS_ACTION");
+                PendingIntent pi = PendingIntent.getBroadcast(MainActivity.this, 0, sendIntent, 0);
+                smsManager.sendTextMessage(to.getText().toString(), null,
+                        msgInput.getText().toString(), pi, null);
+            }
+        });
     }
 
     @Override
@@ -62,6 +94,18 @@ public class MainActivity extends AppCompatActivity {
 
             sender.setText(address);
             content.setText(fullMessage);
+        }
+    }
+
+    class SendStatusReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(getResultCode() == RESULT_OK) {
+                Toast.makeText(context, "Send succeed", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(context, "Send failed", Toast.LENGTH_LONG).show();
+            }
         }
     }
 }
